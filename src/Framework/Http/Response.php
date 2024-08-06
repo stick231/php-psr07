540cc5766837270276bbc039b2bd163e92496c15
stick231 <veniamin7766z@gmail.com>
2024-08-06 18:19:36 +0300

    add response

--- src/Framework/Http/Response.php
+++ src/Framework/Http/Response.php
@@ -1,13 +1,86 @@
 <?php
-class Response{
+namespace Framework\Http;
 
-    public function __construct()
+class Response{
+    private $headers = [];
+    private $body;
+    private $statusCode;
+    private $reasonPhrase = "";
+
+    public function __construct($body, $status = 200)
     {
-        
+        $this->body = $body;
+        $this->statusCode = $status;
     }
 
-    public function withHeaders()
+    private static $phrases = [
+        "200" => "OK",
+        "301" => "Moved Permanently",
+        "400" => "Bad Request",
+        "403" => "Forbiddend",
+        "404" => "Not Found",
+        "500" => "Internal Server Error"
+    ];
+    
+    public function getBody()
     {
-        
+        return $this->body;
+    }
+    
+    public function withBody($body)
+    {
+        $new = clone $this;
+        $new = $new->body;
+        return $new;
+    }
+
+    public function getStatuscode()
+    {
+        return $this->statusCode;
+    }
+
+    public function getReasonPhrase()
+    {
+        if(!$this->reasonPhrase && isset(self::$phrases[$this->statusCode])){
+            $this->reasonPhrase = self::$phrases[$this->statusCode]; 
+        }
+    }
+
+    public function withStatus($status, $reasonPhrase):self
+    {
+        $new = clone $this;
+        $new->statusCode = $status;
+        $new->reasonPhrase = $reasonPhrase;
+        return $new;
+    }
+
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+
+    public function hasHeader($header): bool
+    {
+        return isset($this->headers[$header]);
+    }
+
+    public function getHeader($header)
+    {
+        if(!$this->hasHeader($header))
+        {
+            return null;
+        }
+        return $this->headers[$header];
+    }
+
+    public function withHeaders($header, $value):self
+    {
+        $new = clone $this;
+        if($this->hasHeader($header))
+        {
+            unset($new->headers[$header]);
+        }
+        $new->headers[$header] = $value;
+        return $new;
     }
 }
--- www/index.php
+++ www/index.php
@@ -1,14 +1,23 @@
 <?php
 
 use Framework\Http\RequestFactory;
+use Framework\Http\Response;
 
 $filePath = __DIR__ . '/../vendor/autoload.php';
 
 require $filePath;
 
 
-$request = RequestFactory::fromGlobals();
+$requestFabric = (new RequestFactory())::fromGlobals();
 
-$name = ($request->getQueryParams()['name'] ?? 'quest');
+$name = ($requestFabric->getQueryParams()['name'] ?? 'quest');
 
-$response = (new Response("Hello, $name " . '!'))->withHeaders();
+
+$response = (new Response("Hello, $name " . '!'))->withHeaders("X-Developer", "ElisDN");
+
+header("HTTP/1.0" . $response->getStatuscode() . "" .  $response->getReasonPhrase());
+foreach($response->getHeaders() as $name => $value){
+    header($name . ':' . $value);
+}
+
+echo $response->getBody();
